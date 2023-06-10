61cf8e881c42a83eb16e7c0191c5bb6f7376bc2a
KristianSic <sic.kristian@gmail.com>
2023-06-10 12:31:40 +0200

    Updates

---
+++ _app/immutable/chunks/paths.262943ca.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_ysaw1p)==null?void 0:s.base)??"/WhoPaysWhat";var a;const e=((a=globalThis.__sveltekit_ysaw1p)==null?void 0:a.assets)??t;export{e as a,t as b};
